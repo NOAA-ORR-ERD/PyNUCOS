90e2879fe6fddf48bbb6cc830679afbdb4ba4917
Chris Barker <Chris.Barker@noaa.gov>
2016-05-25 14:32:48 -0700

    cleaned up style in tests

--- setup.py
+++ setup.py
@@ -8,12 +8,11 @@ from setuptools import setup
 from unit_conversion import __version__
 
 setup(
-    name = "unit_conversion",
+    name="unit_conversion",
     description='Physical Unit conversion utilities -- units useful for oil and chemical spill response',
     author='Christopher H. Barker',
     author_email='Chris.Barker@noaa.gov',
     url='https://github.com/NOAA-ORR-ERD/PyNUCOS',
-    version = __version__,
-    packages = ['unit_conversion'],
+    version=__version__,
+    packages=['unit_conversion', 'unit_conversion.tests'],
     )
-
