4420d7d783932534fab47d36e87fc3a44fb22111
Chris Barker <Chris.Barker@noaa.gov>
2021-09-21 13:59:28 -0700

    put the "old" unit_conversion in a package

---
+++ nucos/tests/test_old_api.py
@@ -0,0 +1,25 @@
+"""
+a couple tests to make sure the old package name still works
+
+These just happen to be the ones used on ResponseLink :-)
+"""
+
+from math import isclose
+import unit_conversion
+
+
+def test_convert():
+    result = unit_conversion.convert("Volume", 'liter', "gallons", 10.0)
+
+    assert isclose(result, 2.6417205, rel_tol=1e-6)
+
+
+def test_lat_format():
+    assert unit_conversion.lat_long.format_lat(-32.5) == '32° 30.00′ South'
+
+
+def test_lon_format():
+    assert unit_conversion.lat_long.format_lon(-32.5) == '32° 30.00′ West'
+
+
+
---
+++ unit_conversion/__init__.py
@@ -0,0 +1,33 @@
+"""
+This here for backward compatibility only
+
+It intends to present all the names actually used in the old
+unit_conversion namespace
+
+This API is deprecated -- please use the nucos package instead.
+"""
+
+import warnings
+
+from nucos.unit_conversion import *
+
+from nucos import lat_long
+
+from nucos.lat_long import (LatLongConverter,
+                            Latitude,
+                            Longitude,
+                            format_lat,
+                            format_lon,
+                            format_lat_d,
+                            format_lon_d,
+                            format_lat_dm,
+                            format_lon_dm,
+                            format_lat_dms,
+                            format_lon_dms,
+                            )
+
+warnings.warn(DeprecationWarning(
+    "The unit_conversion module is deprecated -- "
+    "import as `nucos` instead"))
+
+
